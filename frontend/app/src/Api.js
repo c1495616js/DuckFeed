d2813e5d7ad1de4777a075c8657f59f28be48ca9
jerrywang761208 <c1495616@gmail.com>
2018-11-19 06:39:39 -0800

    test docker

--- frontend/app/src/Api.js
+++ frontend/app/src/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.API_HOST || 'http://localhost:8000/index.php/';
+// const baseURL = process.env.API_HOST || 'http://localhost:8000/index.php/';
+const baseURL = 'api/index.php/';
 
 const Api = axios.create({
   baseURL
